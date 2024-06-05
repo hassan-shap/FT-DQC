ce2e2363343d6b6e9d5a48aa6b876e2c6e25c2c7
hassan-shap <hassan.shapp@gmail.com>
2024-06-05 19:12:25 +0000

    surface code 3d with loss

--- surface_code_utils.py
+++ surface_code_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import time
 from pymatching import Matching
+import networkx as nx
 
 def surface_code(dx, dy):
     # dx = 2 * d 
@@ -200,3 +201,174 @@ def perf_sim(d, bandwidth, code_details, gen_coh_ratio_list, p_err, Niter= 3000)
     # print(num_errors)
     # print(predicted_logicals_flipped, actual_logicals_flipped)
     return num_errors
+
+def joint_meas_w_loss(code_details, params):
+
+    p_bdy_qubit = params["p_bdy_qubit"]
+    p_bulk_s = params["p_bulk_stab"]
+    p_bdy_s = params["p_bdy_stab"]
+    bandwidth = params["bandwidth"]
+    repetitions = params["repetitions"]
+    Niter = params["Niter"]
+
+    gen_coh_ratio_list = params["gen_coh_ratio"]
+    num_errors = np.zeros(len(gen_coh_ratio_list))
+
+    d = params["code_dist"]
+    dx = params["dx"]
+
+    code_details_2d, code_details_3d = code_details
+    Smat, logicals, bdy, qubit_idx = code_details_2d
+    num_s, num_q = Smat.shape
+    data_bdy, ancilla_bdy = bdy
+
+    Smat_spacetime, bdy_spacetime, qubit_idx_mat = code_details_3d
+    data_bdy_st, ancilla_bdy_st = bdy_spacetime
+    qubit_idx_spacetime, qubit_coords_spacetime, ancilla_idx_st = qubit_idx_mat
+    num_q_spacetime = Smat_spacetime.shape[1]
+    data_q_idx = list(set(range(num_q_spacetime))-set(ancilla_idx_st))
+
+    # ############## error syndrome calculations ######
+
+    p_s_list = np.ones(num_s) * p_bulk_s
+    p_s_list[ancilla_bdy] = p_bdy_s
+
+
+    for i_t, ratio in enumerate(gen_coh_ratio_list):
+        # print(i_t)
+        
+        if d > bandwidth:
+            idle_time = ((d // bandwidth) * harmonic(bandwidth)  + harmonic (d % bandwidth)  )* ratio
+        else:
+            idle_time = harmonic(d)* ratio
+        p_bulk_qubit = 1 - np.exp(- idle_time )
+        p_qubit_list = np.ones(num_q) * p_bulk_qubit
+        p_qubit_list[data_bdy] = (p_bulk_qubit+ p_bdy_qubit)
+
+        p_qubit_st = np.zeros(num_q_spacetime) 
+        p_qubit_st[data_q_idx] = p_bulk_qubit 
+        p_qubit_st[data_bdy_st] = (p_bulk_qubit+ p_bdy_qubit) 
+        p_qubit_st[ancilla_idx_st] = p_bulk_s
+        p_qubit_st[ancilla_bdy_st] = (p_bulk_s+ p_bdy_s)
+
+        for _ in range(Niter):
+
+            prob_l = params["loss_prob"]
+            error_loss = np.random.rand(num_s , repetitions)
+            error_loss[:,-1] = 1
+            loss_inds_2d = np.argwhere(error_loss < prob_l)
+            loss_inds = np.array([qubit_idx_spacetime[f"({1+ (coords[0]% (dx-1))}, {coords[0]//(dx-1)}, {coords[1]+0.5})"] for coords in loss_inds_2d])
+            remain_inds = np.array(list( set(range(num_q_spacetime)) - set(loss_inds)))
+
+            Sx_red, qubits_to_plot = compute_eff_Sx(Smat_spacetime,loss_inds,remain_inds)
+            overlap = Sx_red.T@Sx_red
+            ql_rep = find_repeated_idx(overlap,qubits_to_plot)
+
+            remain_ancilla = np.intersect1d(remain_inds, ancilla_idx_st)
+            remain_ancilla_bdy = np.intersect1d(remain_inds, ancilla_bdy_st)
+
+            weights = np.zeros(num_q_spacetime)
+            weights[remain_ancilla] = np.log((1-p_bulk_s)/p_bulk_s) 
+            weights[remain_ancilla_bdy] = np.log((1-p_bdy_s)/p_bdy_s) 
+            weights[data_q_idx] = np.log((1-p_bulk_qubit)/p_bulk_qubit) 
+            weights[data_bdy_st] = np.log((1-p_bdy_qubit)/p_bdy_qubit)
+
+            for links in ql_rep:
+                p_eff = (1-np.prod(1-2*p_qubit_st[links]))/2
+                weights[links] = np.log((1-p_eff)/p_eff)
+
+            assert len(np.argwhere(weights>0))== len(qubits_to_plot)
+
+            matching_st = Matching(Smat_spacetime,spacelike_weights=weights)
+
+            noise_new = (np.random.rand(num_q, repetitions) < np.reshape(p_qubit_list,(p_qubit_list.shape[0],1))).astype(np.uint8)
+            noise_cumulative = (np.cumsum(noise_new, 1) % 2).astype(np.uint8)
+            noise_total = noise_cumulative[:,-1]
+            syndrome = Smat@noise_cumulative % 2
+
+            syndrome_error = (np.random.rand(num_s, repetitions) < np.reshape(p_s_list,(p_s_list.shape[0],1)) ).astype(np.uint8)
+            syndrome_error[:,-1] = 0 # Perfect measurements in last round to ensure even parity
+            noisy_syndrome = (syndrome + syndrome_error) % 2
+            # # Convert to difference syndrome
+            noisy_syndrome[:,1:] = (noisy_syndrome[:,1:] - noisy_syndrome[:,0:-1]) % 2
+
+            recovery_st = matching_st.decode(noisy_syndrome)
+            recovery_tot = np.zeros(num_q)
+            for idx, q in enumerate(recovery_st):
+                x, y, t = qubit_coords_spacetime[idx]
+                if t % 1 == 0:
+                    recovery_tot[qubit_idx[f"({x}, {y})"]] += recovery_st[idx]
+            recovery_tot %= 2
+
+            predicted_logicals_flipped = recovery_tot@logicals.T % 2
+            actual_logicals_flipped = noise_total@logicals.T % 2
+            num_errors[i_t] += not (predicted_logicals_flipped == actual_logicals_flipped)
+
+    num_errors /= Niter
+
+    return num_errors
+
+##################
+def compute_eff_Sx(Sx,loss_inds,remain_inds):
+    G_loss = nx.Graph()
+    for loss_index in loss_inds:
+        s_inds = np.argwhere(Sx[:,loss_index]>0)[:,0]
+        # if len(s_inds)>1:
+        G_loss.add_edge(s_inds[0],s_inds[1])
+        # else:
+        #     G_loss.add_node(s_inds[0])
+
+    components = [G_loss.subgraph(c).copy() for c in nx.connected_components(G_loss)]
+    lost_vs = []
+    for i_c, c in enumerate(components):
+        lost_vs += c.nodes()
+
+    num_s = Sx.shape[0]
+    remain_vs = list(set(range(num_s)) - set(lost_vs))
+    num_stab = len(components)+len(remain_vs)
+    Sx_red2 = np.zeros((num_stab,len(remain_inds)))
+    Sx_red2[len(components):,:] = Sx[np.ix_(remain_vs,remain_inds)]
+    for i_c, c in enumerate(components):
+        Sx_red2[i_c,:] = np.sum(Sx[np.ix_(c.nodes(),remain_inds)],axis = 0)%2
+
+    keep_cols = np.argwhere(np.sum(Sx_red2,axis=0)>0)[:,0]
+    Sx_red2 = Sx_red2[:,keep_cols]
+    qubits_to_plot = remain_inds[keep_cols]
+
+    return Sx_red2, qubits_to_plot
+
+def find_repeated_idx(overlap,qubits_to_plot):
+    inds = np.argwhere(overlap>1)
+    if len(inds)>0:
+        rep_edges = []
+        for i_v in inds:
+            if i_v[1]>i_v[0]:
+                if not (i_v[0] in rep_edges):
+                    rep_edges.append(i_v[0])
+                if not (i_v[1] in rep_edges):
+                    rep_edges.append(i_v[1])
+
+        rep_edges = np.sort(rep_edges).astype(int)
+
+        repeated_inds = []
+        counter = 0
+        i = 0 
+        overlap2 = overlap[np.ix_(rep_edges,rep_edges)]
+        inds_to_keep2 = list(range(len(rep_edges)))
+        while counter < len(rep_edges):
+            edge = inds_to_keep2[i]
+            ovlp_inds = np.argwhere(overlap2[edge,inds_to_keep2[i+1:]]==2)
+            nl_i = len(ovlp_inds)+1
+
+            qlist = qubits_to_plot[rep_edges[np.ix_([ inds_to_keep2[k] for k in i+1+ovlp_inds[:,0]])]]
+            for j in ovlp_inds[::-1,0]:
+                inds_to_keep2.remove(inds_to_keep2[i+1+j])
+            repeated_inds.append(list(np.concatenate(([qubits_to_plot[rep_edges[edge]]],qlist))))
+            counter += nl_i
+            i += 1
+
+    else:
+        repeated_inds = []
+
+    return repeated_inds
+##################
